ad2433cc015498899de7c081460ebcc48f9ce4b9
feit <i@feit.me>
2016-04-17 19:38:31 +0800

    support import unused cards

--- src/scripts/queryUnused.js
+++ src/scripts/queryUnused.js
@@ -1,11 +1,25 @@
 const Datastore = require('nedb');
 const path = require('path');
+const async = require('async');
 
 const Cards = new Datastore({ filename: path.join(__dirname, 'cards.db'), autoload: true });
 
-Cards.find({ value: 50, isChecked: true, isCharged: false }, (err, cards) => {
+Cards.find({ isChecked: true, isCharged: false }, (err, cards) => {
   if (err) return console.error(err);
 
-  console.log(cards);
-  console.log(cards.length);
+  const batches = {};
+  const values = {
+    20: 0,
+    30: 0,
+    50: 0,
+  };
+  async.each(cards, (card, callback) => {
+    batches[card.batch] = batches[card.batch] || 0;
+    batches[card.batch] += 1;
+    values[card.value] += 1;
+    callback();
+  }, () => {
+    console.log(batches);
+    console.log(values);
+  });
 });
--- src/scripts/updateUnused.js
+++ src/scripts/updateUnused.js
@@ -1,7 +1,12 @@
 const Datastore = require('nedb');
 const path = require('path');
+const async = require('async');
 
 import Models from '../models';
+import crypto from '../lib/crypto';
+import config from '../../config';
+
+const cryptor = crypto(config.ekey);
 
 const { NetCard } = Models;
 
@@ -10,15 +15,28 @@ const Cards = new Datastore({ filename: path.join(__dirname, 'cards.db'), autolo
 Cards.find({ isChecked: true, isCharged: false }, (err, cards) => {
   if (err) return console.error(err);
 
-  cards.forEach(card => {
-    NetCard.update({ ka: card.ka }, {
-      $set: {
-        isUsed: false,
-        orderID: '',
-      },
-    }, (e1, row) => {
-      if (e1) console.error(err);
-      if (!row.nModified) console.log(`Card Not Found: ${card.ka}`);
+  async.each(cards, (card, callback) => {
+    NetCard.findOne({ ka: card.ka }, (e1, targetCard) => {
+      if (e1) return console.error(e1);
+
+      if (!targetCard) {
+        console.log('not found: ' + card.ka);
+        NetCard.create({
+          ka: card.ka,
+          mi: cryptor.encrypt(card.ka, card.mi),
+          value: card.value,
+          expireAt: card.expireAt,
+        }, callback);
+      } else {
+        NetCard.update({ ka: card.ka }, {
+          $set: {
+            isUsed: false,
+            orderID: '',
+          },
+        }, callback);
+      }
     });
+  }, () => {
+    console.log('done');
   });
 });
